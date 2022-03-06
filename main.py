0162796aad4e2272a71278161e516d48b80f4f0d
Seonwoo Park <seonoo00@naver.com>
2022-03-06 23:45:25 +0900

    2022-03-07 commit

--- main.py
+++ main.py
@@ -6,7 +6,17 @@
 import numpy as np
 import cv2
 
-print("Hello World!")
+imp_basic = cv2.imread('cat.jpg', cv2.IMREAD_COLOR)
+cv2.imshow('Image Cat', imp_basic)
+cv2.waitKey(0)
+cv2.imwrite('result1.jpg', imp_basic)
+
+cv2.destroyAllWindows()
+
+imp_gray = cv2.cvtColor(imp_basic, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Image Gray Cat', imp_gray)
+cv2.waitKey(0)
+cv2.imwrite('result2.jpg', imp_gray)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
